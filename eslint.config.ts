45b91fce46204369ec6523ddd551d39f6abcba32
Fan <fanj.zh@foxmail.com>
2026-03-11 10:08:51 +0800

    Fix.

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import {defineConfig} from "eslint/config";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
